44c69b0f7b0e9d50070c1221c1fefbad5eb20c54
OLGAAPAK <olga.pak777@yandex.ru>
2024-01-28 20:07:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums1 = new int[]{2, 3, 4, 2, 4, 6, 10};
+        System.out.println(removeDuplicates(nums1));
+    }
+    public static int removeDuplicates(int[] nums){
+        int result = nums.length;
+        HashSet<Integer> states = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!states.add(nums[i])){
+                result--;
+            }
+        }
+        return result;
+    }
+}
